9572d970a671f863ddbd7ba750378d3590167fa0
omer saar <omersaar2104@gmail.com>
2020-03-03 19:17:35 +0200

    initial commit: loading resources necessary for project

---
+++ src/assets/index.js
@@ -0,0 +1,17 @@
+import emptyMarkedURL from "./empty-marked.png"
+import blackBallURL from "./black-ball.png"
+import blackBallMarkedURL from "./black-marked.png"
+import blackBallYellowURL from "./black-ball-light-yellow.png"
+import whiteBallURL from "./white-ball.png"
+import whiteBallMarkedURL from "./white-marked.png"
+import whiteBallYellowURL from "./white-ball-light-yellow.png"
+
+export {
+    emptyMarkedURL,
+    blackBallURL,
+    blackBallMarkedURL,
+    blackBallYellowURL,
+    whiteBallURL,
+    whiteBallMarkedURL,
+    whiteBallYellowURL
+}
---
+++ src/components/BoardGame/BoardGame.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const BoardGame = props => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+BoardGame.propTypes = {
+
+}
+
+export default BoardGame
